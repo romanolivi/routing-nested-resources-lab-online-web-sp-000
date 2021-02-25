af89eae360b76ae2dac580c7ea5ab3153aa61f37
romanolivi <romano_livi@outlook.com>
2021-02-24 20:40:36 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,29 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id] 
+      if Artist.where(id: params[:artist_id]).present?
+        @songs = Artist.find(params[:artist_id]).songs
+      else 
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path 
+      end
+    else 
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      if Song.where(id: params[:id]).present?
+        @song = Song.find(params[:id])
+      else 
+        @artist = Artist.find(params[:artist_id])
+        flash[:alert] = "Song not found."
+        redirect_to artist_songs_path(@artist)
+      end
+    else 
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
+
+  resources :artists, only: [:show, :index, :new, :create, :edit, :update, :destroy] do 
+    resources :songs, only: [:show, :index]
+  end 
+
   resources :songs
 end
